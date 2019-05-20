d1fb30fa16b2646cd27832174244e4ccc7e6ad98
DILEEP-YADAV <dps.online24h@gmail.com>
2019-05-20 09:05:12 +0530

    Add files via upload
    good

---
+++ SakDatePicker/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'SakDatePicker';
+  en: any;
+  value: Date;
+  constructor() {
+    this.value = new Date();
+  }
+  ngOnInit() {
+    this.en = {
+      firstDayOfWeek: 0,
+      dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      dayNamesMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+      // tslint:disable-next-line:max-line-length
+      monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      today: 'Today',
+      clear: 'Clear'
+    };
+  }
+
+}
